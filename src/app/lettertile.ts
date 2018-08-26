ad0f873fb3e048d2bd59ca95c53c27b5ee265184
Rob van Daal <rvandaal@hotmail.com>
2018-08-26 18:09:48 +0200

    added letter values

--- src/app/lettertile.ts
+++ src/app/lettertile.ts
@@ -8,4 +8,29 @@ export class LetterTile {
         this.col = col;
         this.letter = letter;
     }
+
+    get value(): number {
+        if (['A', 'E', 'N', 'O'].includes(this.letter)) {
+            return 1;
+        }
+        if (['D', 'I', 'R', 'S', 'T', 'U'].includes(this.letter)) {
+            return 2;
+        }
+        if (['G', 'K', 'L', 'M'].includes(this.letter)) {
+            return 3;
+        }
+        if (['B', 'F', 'H', 'J', 'P', 'V'].includes(this.letter)) {
+            return 4;
+        }
+        if (['C', 'W', 'Z'].includes(this.letter)) {
+            return 5;
+        }
+        if (['X', 'Y'].includes(this.letter)) {
+            return 8;
+        }
+        if (['Q'].includes(this.letter)) {
+            return 10;
+        }
+        return 0;
+    }
 }
